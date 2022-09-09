eb0b46dcaeddfe079735d772659f3c6c425ee8a6
roge2401 <3jW1UTcojrs0>
2022-09-09 15:05:37 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'painting-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

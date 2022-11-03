5878c419c6be9e76749979b253b5940884a68540
roge2401 <3jW1UTcojrs0>
2022-11-03 12:03:30 -0600

    correcciones de ejecución

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,30 +12,25 @@ import { AppRoutingModule } from './app-routing.module';
 * */
 import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
 import {SocialSharing} from '@awesome-cordova-plugins/social-sharing/ngx';
-import { PDFGenerator } from '@ionic-native/pdf-generator/ngx';
+//import { PDFGenerator } from '@ionic-native/pdf-generator/ngx';
 
 
 // imports
-import { File } from '@ionic-native/file/ngx';
-import { FileOpener } from '@ionic-native/file-opener/ngx';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+//import { File } from '@ionic-native/file/ngx';
+//import { FileOpener } from '@ionic-native/file-opener/ngx';
+//import { ServiceWorkerModule } from '@angular/service-worker';
+//import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: environment.production,
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     Base64ToGallery,
     SocialSharing,
-    PDFGenerator,
-    File,
-    FileOpener,
+    //PDFGenerator,
+    //File,
+    //FileOpener,
   ],
   bootstrap: [AppComponent],
 })
